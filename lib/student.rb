a7d47e397dfdce57232a2288ab08245e4f766593
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 22:19:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,10 +92,10 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE COUNT(?) grade = ?
+      WHERE grade = ?
       SQL
     
-    DB[:conn].execute(sql, num, 10).map do |row|
+    DB[:conn].execute(sql, 10).map do |row|
       self.new_from_db(row)
     end
   end
