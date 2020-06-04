931bcb54ede35276e2e931553bbfdf9091c1ee05
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 22:18:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,14 +100,15 @@ class Student
     end
   end
     
-  def self.first_student_in_grade_10()
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = ?
+      LIMIT 1
       SQL
     
-      DB[:conn].execute(sql, 9).map do |row|
+      DB[:conn].execute(sql, 10).map do |row|
         self.new_from_db(row)
       end
     end
