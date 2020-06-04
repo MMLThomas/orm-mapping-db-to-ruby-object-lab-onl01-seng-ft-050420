45718416b762369a1559084d4660e5969d40d500
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 22:39:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,9 +96,10 @@ class Student
       LIMIT ?
       SQL
     
-    10th_grade = DB[:conn].execute(sql, 10, num).map do |row|
+      DB[:conn].execute(sql, 10, num).map do |row|
       self.new_from_db(row)
     end
+    
   end
     
   def self.first_student_in_grade_10
